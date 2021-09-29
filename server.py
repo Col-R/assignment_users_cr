7c34cd13804e6481da59ead7d5dd225569c169b6
Col-R <2colewestin@gmail.com>
2021-09-29 18:50:29 -0500

    added routes for taking input and displaying data

--- server.py
+++ server.py
@@ -1,12 +1,34 @@
 from flask import Flask, render_template, request, session, redirect
 # import the class from friend.py
-from user import User
+from user import Users
 app = Flask(__name__)
+
 @app.route("/")
 def index():
-    # call the get all classmethod to get all friends
-    users = User.get_all()
-    print(users)
-    return render_template("index.html", all_users = users)
+    return redirect('/users')
+
+@app.route('/users')
+def users():
+    return render_template('index.html', users=Users.get_all())
+
+@app.route('/users/new')
+def new():
+    return render_template('create.html')
+@app.route('/create_user', methods=["POST"])
+def create_user():
+    print (request.form)
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "email" : request.form["email"]
+    }
+    # We pass the data dictionary into the save method from the Friend class.
+    Users.save(data)
+    # Don't forget to redirect after saving to the database.
+    return redirect('/users')
+
+# @app.route('result')
+# def result():
+
 if __name__ == "__main__":
     app.run(debug=True)
